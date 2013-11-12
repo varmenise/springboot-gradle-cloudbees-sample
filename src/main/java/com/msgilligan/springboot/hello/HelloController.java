1e792753a133f04deb3809624acb362c4b969981
Sean Gilligan <sean@msgilligan.com>
2013-11-12 13:11:39 -0800

    First working version that deploys to CloudBees

---
+++ src/main/java/com/msgilligan/springboot/hello/HelloController.java
@@ -0,0 +1,14 @@
+package com.msgilligan.springboot.hello;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot and CloudBees!";
+    }
+
+}
