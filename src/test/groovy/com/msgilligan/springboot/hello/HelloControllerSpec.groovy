378c4dc9487f8c0094ef140a6244fe766eb2c445
Sean Gilligan <sean@msgilligan.com>
2014-04-01 15:57:37 -0700

    Remove unused imports

--- src/test/groovy/com/msgilligan/springboot/hello/HelloControllerSpec.groovy
+++ src/test/groovy/com/msgilligan/springboot/hello/HelloControllerSpec.groovy
@@ -1,18 +1,6 @@
 package com.msgilligan.springboot.hello
 
-import org.springframework.boot.SpringApplication
-import org.springframework.context.ConfigurableApplicationContext
-import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseEntity
-import org.springframework.web.client.RestTemplate
-import spock.lang.AutoCleanup
-import spock.lang.Shared
 import spock.lang.Specification
-
-import java.util.concurrent.Callable
-import java.util.concurrent.Executors
-import java.util.concurrent.Future
-import java.util.concurrent.TimeUnit
 
 /**
  * Spock Unit Test Specification
